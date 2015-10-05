bc202bbaa190f81b4175aed5c09c5f2e0769999d
James Cool <jeffreysbrother@gmail.com>
2015-10-05 16:02:55 -0700

    added the response() function and an additional set of modals

--- dev/js/script.js
+++ dev/js/script.js
@@ -3,27 +3,32 @@ function reveal1() {
   document.getElementById("button2").disabled = true;
   document.getElementById("button3").disabled = true;
 }
+
 function reveal2() {
   document.getElementById('modal2').style.display = 'block';
   document.getElementById("button1").disabled = true;
   document.getElementById("button3").disabled = true;
 }
+
 function reveal3() {
   document.getElementById('modal3').style.display = 'block';
   document.getElementById("button1").disabled = true;
   document.getElementById("button2").disabled = true;
 }
+
 function refresh() {
   location.reload();
 }
 
-// function response(){
-//   var x = document.getElementById("one").value;
-//   var y = document.getElementById("two").value;
-//   var z = document.getElementById("three").value;
-//
-//   if (x === )
-// }
+function response() {
+  if(document.getElementById('one').checked === true) {
+    document.getElementById('modal4').style.display = 'block';
+  } else if(document.getElementById('two').checked === true) {
+    document.getElementById('modal5').style.display = 'block';
+  } else if(document.getElementById('three').checked === true) {
+    document.getElementById('modal6').style.display = 'block';
+  }
+}
 
 document.getElementById("button1").addEventListener("click", reveal1);
 document.getElementById("button2").addEventListener("click", reveal2);
