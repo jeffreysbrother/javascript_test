2f856dbbb4062ed83c8306cf880937bea6bcdaa1
James Cool <jeffreysbrother@gmail.com>
2015-10-05 23:21:35 -0700

    replaced show() and css() functions with slideDown()

--- dev/js/jquery.js
+++ dev/js/jquery.js
@@ -5,30 +5,30 @@ function refresh() {
 
 //action for initial button click
 $('#button1').click(function(){
-  $('#modal1').show();
+  $('#modal1').slideDown("slow");
   $('#button2, #button3').prop('disabled', true);
 });
 
 $('#button2').click(function(){
-  $('#modal2').show();
+  $('#modal2').slideDown("slow");
   $('#button1, #button3').prop('disabled', true);
 });
 
 $('#button3').click(function(){
-  $('#modal3').show();
+  $('#modal3').slideDown("slow");
   $('#button1, #button2').prop('disabled', true);
 });
 
 //conditional based on whether a radio button is "checked"
 function response() {
   if($('#one').is(':checked') || $('#one-1').is(':checked') || $('#one-one').is(':checked')) {
-    $('#modal4').css("display", "block");
+    $('#modal4').slideDown("slow");
     $('.submit-button').prop('disabled', true);
   } else if($('#two').is(':checked') || $('#two-2').is(':checked') || $('#two-two').is(':checked')) {
-    $('#modal5').css("display", "block");
+    $('#modal5').slideDown("slow");
     $('.submit-button').prop('disabled', true);
   } else if($('#three').is(':checked') || $('#three-3').is(':checked') || $('#three-three').is(':checked')) {
-    $('#modal6').css("display", "block");
+    $('#modal6').slideDown("slow");
     $('.submit-button').prop('disabled', true);
   }
 }
