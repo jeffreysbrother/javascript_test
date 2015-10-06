b9c21539b3de0dc135a60c286364d54b38bfc891
James Cool <jeffreysbrother@gmail.com>
2015-10-05 22:13:26 -0700

    implemented conditional in jQuery

--- dev/js/jquery.js
+++ dev/js/jquery.js
@@ -3,6 +3,7 @@ function refresh() {
   location.reload();
 }
 
+//action for initial button click
 $('#button1').click(function(){
   $('#modal1').show();
   $('#button2, #button3').prop('disabled', true);
@@ -17,3 +18,14 @@ $('#button3').click(function(){
   $('#modal3').show();
   $('#button1, #button2').prop('disabled', true);
 });
+
+//conditional based on whether a radio button is "checked"
+function response() {
+  if($('#one').is(':checked') || $('#one-1').is(':checked') || $('#one-one').is(':checked')) {
+    $('#modal4').css("display", "block");
+  } else if($('#two').is(':checked') || $('#two-2').is(':checked') || $('#two-two').is(':checked')) {
+    $('#modal5').css("display", "block");
+  } else if($('#three').is(':checked') || $('#three-3').is(':checked') || $('#three-three').is(':checked')) {
+    $('#modal6').css("display", "block");
+  }
+}
