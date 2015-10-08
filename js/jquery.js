7ebfe0bd583cfe7c4e37deb74ce04ecfe038ab8c
James Cool <jeffreysbrother@gmail.com>
2015-10-08 01:43:17 -0700

    changed size of large error for mobile, and implemented scroll to element

--- js/jquery.js
+++ js/jquery.js
@@ -24,12 +24,21 @@ function response() {
   if($('#one').is(':checked') || $('#one-1').is(':checked') || $('#one-one').is(':checked')) {
     $('#modal4').slideDown("slow");
     $('.submit-button').prop('disabled', true);
+    $('html, body').animate({
+      scrollTop: $("#modal4").offset().top
+    }, 1000);
   } else if($('#two').is(':checked') || $('#two-2').is(':checked') || $('#two-two').is(':checked')) {
     $('#modal5').slideDown("slow");
     $('.submit-button').prop('disabled', true);
+    $('html, body').animate({
+      scrollTop: $("#modal5").offset().top
+    }, 1000);
   } else if($('#three').is(':checked') || $('#three-3').is(':checked') || $('#three-three').is(':checked')) {
     $('#modal6').slideDown("slow");
     $('.submit-button').prop('disabled', true);
+    $('html, body').animate({
+      scrollTop: $("#modal6").offset().top
+    }, 1000);
   }
 }
 
@@ -38,10 +47,16 @@ $('#submit-button-2').click(function(){
   //the below expression will check if the textarea is blank or filled only with blank spaces
   if (!$.trim($('.textarea1').val())) {
     $('.error').show();
+    $('html, body').animate({
+      scrollTop: $(".error").offset().top
+    }, 1000);
   } else if ($('.textarea1').val()) {
     $('.error').hide();
     $('#sucks').show();
     $('#submit-button-2').prop('disabled', true);
+    $('html, body').animate({
+      scrollTop: $("#sucks").offset().top
+    }, 1000);
   }
 });
 
@@ -50,10 +65,16 @@ $('#submit-button-3').click(function(){
   //the below expression will check if the textarea is blank or filled only with blank spaces
   if (!$.trim($('.textarea2').val())) {
     $('.error').show();
+    $('html, body').animate({
+      scrollTop: $(".error").offset().top
+    }, 1000);
   } else if ($('.textarea2').val()) {
     $('.error').hide();
     $('#sucks').show();
     $('#submit-button-3').prop('disabled', true);
+    $('html, body').animate({
+      scrollTop: $("#sucks").offset().top
+    }, 1000);
   }
 });
 
